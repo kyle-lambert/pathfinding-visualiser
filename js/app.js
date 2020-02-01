1f73eecf3d92daec600ee19ece0767581ab3d7ef
kylelambert001 <kylelambert001@gmail.com>
2020-02-01 22:18:04 +0900

    bug fixes

--- js/app.js
+++ js/app.js
@@ -23,36 +23,73 @@ const controlGrid = () => {
   // render new grid
   gridView.displayGrid(state.grid.grid);
 
-  console.log(state.grid);
+  // console.log(state.grid);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   controlGrid();
+  setupEventListeners();
 })
 
-document.querySelector('.grid__container').addEventListener('click', (e) => {
-  const id = e.target.id;
-  const row = e.target.dataset.row;
-  const col = e.target.dataset.col;
+// document.querySelector('.grid__container').addEventListener('click', (e) => {
+//   const id = e.target.id;
+//   const row = e.target.dataset.row;
+//   const col = e.target.dataset.col;
 
-  console.log(row, col);
+//   // check make sure user hasnt click on a start or target node
+//   if (!state.grid.isStartNode(id) && !state.grid.isTargetNode(id)) {
+//     if (state.grid.isWallNode(id)) {
+//       // remove wall
+//       state.grid.removeWall(row, col);
+//     } else {
+//       // add wall
+//       state.grid.addWall(row, col);
+//     }
+//     // prepare UI for grid
+//     gridView.clearGrid();
 
-  // check make sure user hasnt click on a start or target node
-  if (!state.grid.isStartNode(id) && !state.grid.isTargetNode(id)) {
-    if (state.grid.isWallNode(id)) {
-      // remove wall
-      state.grid.removeWall(row, col);
-    } else {
-      // add wall
-      state.grid.addWall(row, col);
+//     // display updated grid
+//     gridView.displayGrid(state.grid.grid);
+
+//     console.log(state.grid)
+//   }
+
+// })
+
+const setupEventListeners = () => {
+  document.querySelector('.grid__container').addEventListener('mousedown', (e) => {
+    if (e.target.matches('.tile')) {
+      if (e.target.className.includes('tile--wall')) {
+        e.target.classList.remove('tile--wall');
+      } else {
+        e.target.classList.add('tile--wall');
+      }
     }
-    // prepare UI for grid
-    gridView.clearGrid();
 
-    // display updated grid
-    gridView.displayGrid(state.grid.grid);
+  })
 
-    console.log(state.grid.walls)
-  }
+  document.querySelector('.grid__container').addEventListener('mouseover', (e) => {
+    if (e.target.matches('.tile')) {
+      if (e.buttons === 1) {
+        if (e.target.className.includes('tile--wall')) {
+          e.target.classList.remove('tile--wall');
+        } else {
+          e.target.classList.add('tile--wall');
+        }
+      }
+    }
+  })
 
-})
+  document.querySelector('.grid__container').addEventListener('click', (e) => {
+    document.querySelectorAll('.tile').forEach(tile => {
+      if (tile.className.includes('tile--wall')) {
+        const row = tile.dataset.row;
+        const col = tile.dataset.col;
+
+
+      }
+    })
+  })
+
+
+}
